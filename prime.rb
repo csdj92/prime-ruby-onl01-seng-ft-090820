054135b1d2346dd7e308dc8cae465ba693613c6e
CJ Davis <csdj92@gmail.com>
2020-09-10 21:07:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(prime_number)
   i=2
 if prime_number > 1
-  range = (i..prime_number-1)to_a
+  range = (i..prime_number-1).to_a
   range.none? {|test_prime| prime_number % test_prime==0}
 end
 end
