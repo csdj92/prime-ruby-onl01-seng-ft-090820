66ca1e3c6c91c6dd332d258b4d7f321c41b76392
CJ Davis <csdj92@gmail.com>
2020-09-10 21:10:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,7 @@ def prime?(prime_number)
   i=2
 if prime_number > 1
   range = (i..prime_number-1).to_a
-  range.none? {|test_prime| prime_number % test_prime==0}
+  range.none? {|test_prime| prime_number % test_prime==0 else false}
 end
-else false
 end
 end
